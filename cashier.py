c8629aa307376330f2ac95b0741da2677e6b851e
sparksys98 <hanineiroukh@gmail.com>
2020-02-06 09:43:20 +0200

    Best cashier system ever

--- cashier.py
+++ cashier.py
@@ -1,5 +1,25 @@
 def main():
-	# write code here
+	dic={}
+	items=[]
+	while True:
+		name=str(input("Items: enter \"done\" when finished: "))
+		if(name=="done"):
+			break
+		price=float(input("Price: "))
+		quantity=int(input("Quantity:" ))
+
+		dic["name"] = name
+		dic["price"] = price
+		dic["quantity"] = quantity
+		price=dic["price"]*dic["quantity"]
+		items.append(dic.copy())
+		total2=0
+		for num in items:
+			total=num["quantity"]*num["price"]
+			print(num["quantity"] , num["name"] ,total)
+			total2=total2+total
+
+		print("Total is: ", total2)
 
 if __name__ == '__main__':
 	main()
